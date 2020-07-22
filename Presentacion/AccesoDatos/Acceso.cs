ebb85633b65dc56aa62a0e0df65282bb65ac8dca
Joiner Calderon <jcvalle_13@hotmail.com>
2020-07-22 00:53:19 -0600

    Se crea los atributos y constructor para acceso a base de datos(Proyecto1_Globo). Al atributo se configura el strconexion

---
+++ Presentacion/AccesoDatos/Acceso.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.Common;
+using System.Data;
+using Entidades;
+
+namespace AccesoDatos
+{
+    public class Acceso
+    {
+        #region Atributos
+        //Configuracion a base de datos Joiner Proyecto1_Globo
+        private string strconexion = Properties.Settings.Default.Proyecto1_Globo;
+        private SqlConnection objconexion;
+        #endregion
+
+        #region Constructor
+
+        public Acceso()
+        {
+            try
+            {
+                //Inicializa la conexion con la cadena 
+                objconexion = new SqlConnection(strconexion);
+                this.ABRIRCONEXION();  //se abre conexión
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                this.CERRARCONEXION(); //Cierre de conexión
+            }
+        }
+        #endregion
+
+        #region Metodos privados
+
+        private void ABRIRCONEXION()
+        {
+            if (objconexion.State == System.Data.ConnectionState.Closed)
+                objconexion.Open();
+        }
+
+        private void CERRARCONEXION()
+        {
+            if (objconexion.State == System.Data.ConnectionState.Open)
+                objconexion.Close();
+        }
+
+        #endregion
+
+
+
+    }
+}
