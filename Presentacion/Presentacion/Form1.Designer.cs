7ea5acbeddb6df378cf522ee353230fa7fe7a3a6
Joiner Calderon <jcvalle_13@hotmail.com>
2020-07-22 00:04:20 -0600

    Se crea formulario Creacion de Usuarios Se crea carpeta recursos

--- Presentacion/Presentacion/Form1.Designer.cs
+++ Presentacion/Presentacion/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Presentacion
 {
-    partial class FrmLogin
+    partial class CrearUsuario
     {
         /// <summary>
         /// Required designer variable.
@@ -28,106 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmLogin));
-            this.lblBloqueo = new System.Windows.Forms.Label();
-            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
-            this.btnIngreso = new System.Windows.Forms.Button();
-            this.txtClave = new System.Windows.Forms.TextBox();
-            this.txtUsuario = new System.Windows.Forms.TextBox();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CrearUsuario));
+            this.cboPerfiles = new System.Windows.Forms.ComboBox();
+            this.txtNombreUsuario = new System.Windows.Forms.TextBox();
+            this.txtClaveAcceso = new System.Windows.Forms.TextBox();
+            this.btnCrear = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // lblBloqueo
+            // cboPerfiles
             // 
-            this.lblBloqueo.AutoSize = true;
-            this.lblBloqueo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblBloqueo.ForeColor = System.Drawing.Color.Red;
-            this.lblBloqueo.Location = new System.Drawing.Point(16, 75);
-            this.lblBloqueo.Name = "lblBloqueo";
-            this.lblBloqueo.Size = new System.Drawing.Size(0, 16);
-            this.lblBloqueo.TabIndex = 13;
+            this.cboPerfiles.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboPerfiles.FormattingEnabled = true;
+            this.cboPerfiles.Location = new System.Drawing.Point(136, 113);
+            this.cboPerfiles.Name = "cboPerfiles";
+            this.cboPerfiles.Size = new System.Drawing.Size(100, 21);
+            this.cboPerfiles.TabIndex = 15;
             // 
-            // linkLabel1
+            // txtNombreUsuario
             // 
-            this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(16, 151);
-            this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(71, 13);
-            this.linkLabel1.TabIndex = 12;
-            this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "Crear Usuario";
+            this.txtNombreUsuario.Location = new System.Drawing.Point(136, 76);
+            this.txtNombreUsuario.MaxLength = 50;
+            this.txtNombreUsuario.Name = "txtNombreUsuario";
+            this.txtNombreUsuario.Size = new System.Drawing.Size(311, 20);
+            this.txtNombreUsuario.TabIndex = 14;
             // 
-            // btnIngreso
+            // txtClaveAcceso
             // 
-            this.btnIngreso.Location = new System.Drawing.Point(102, 97);
-            this.btnIngreso.Name = "btnIngreso";
-            this.btnIngreso.Size = new System.Drawing.Size(84, 37);
-            this.btnIngreso.TabIndex = 11;
-            this.btnIngreso.Text = "Ingresar";
-            this.btnIngreso.UseVisualStyleBackColor = true;
+            this.txtClaveAcceso.Location = new System.Drawing.Point(136, 39);
+            this.txtClaveAcceso.MaxLength = 20;
+            this.txtClaveAcceso.Name = "txtClaveAcceso";
+            this.txtClaveAcceso.PasswordChar = '*';
+            this.txtClaveAcceso.Size = new System.Drawing.Size(100, 20);
+            this.txtClaveAcceso.TabIndex = 13;
             // 
-            // txtClave
+            // btnCrear
             // 
-            this.txtClave.Location = new System.Drawing.Point(102, 50);
-            this.txtClave.Name = "txtClave";
-            this.txtClave.PasswordChar = '*';
-            this.txtClave.Size = new System.Drawing.Size(100, 20);
-            this.txtClave.TabIndex = 10;
+            this.btnCrear.BackColor = System.Drawing.SystemColors.Control;
+            this.btnCrear.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnCrear.BackgroundImage")));
+            this.btnCrear.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnCrear.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCrear.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnCrear.Location = new System.Drawing.Point(136, 151);
+            this.btnCrear.Name = "btnCrear";
+            this.btnCrear.Size = new System.Drawing.Size(67, 71);
+            this.btnCrear.TabIndex = 12;
+            this.btnCrear.Text = "Crear";
+            this.btnCrear.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnCrear.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnCrear.UseVisualStyleBackColor = false;
             // 
-            // txtUsuario
+            // label4
             // 
-            this.txtUsuario.Location = new System.Drawing.Point(102, 21);
-            this.txtUsuario.Name = "txtUsuario";
-            this.txtUsuario.Size = new System.Drawing.Size(100, 20);
-            this.txtUsuario.TabIndex = 9;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Tipo Perfil";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Nombre de Usuario";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 58);
+            this.label2.Location = new System.Drawing.Point(9, 46);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(34, 13);
-            this.label2.TabIndex = 8;
-            this.label2.Text = "Clave";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Clave de Acceso";
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 28);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(43, 13);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "Usuario";
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.pictureBox1.Location = new System.Drawing.Point(258, 82);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(74, 82);
-            this.pictureBox1.TabIndex = 14;
-            this.pictureBox1.TabStop = false;
-            // 
-            // FrmLogin
+            // CrearUsuario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(375, 193);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.lblBloqueo);
-            this.Controls.Add(this.linkLabel1);
-            this.Controls.Add(this.btnIngreso);
-            this.Controls.Add(this.txtClave);
-            this.Controls.Add(this.txtUsuario);
+            this.ClientSize = new System.Drawing.Size(457, 234);
+            this.Controls.Add(this.cboPerfiles);
+            this.Controls.Add(this.txtNombreUsuario);
+            this.Controls.Add(this.txtClaveAcceso);
+            this.Controls.Add(this.btnCrear);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Name = "FrmLogin";
-            this.Text = "Login";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.Name = "CrearUsuario";
+            this.Text = "Creación de Usuarios";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -135,13 +128,12 @@
 
         #endregion
 
-        private System.Windows.Forms.Label lblBloqueo;
-        private System.Windows.Forms.LinkLabel linkLabel1;
-        private System.Windows.Forms.Button btnIngreso;
-        private System.Windows.Forms.TextBox txtClave;
-        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.ComboBox cboPerfiles;
+        private System.Windows.Forms.TextBox txtNombreUsuario;
+        private System.Windows.Forms.TextBox txtClaveAcceso;
+        private System.Windows.Forms.Button btnCrear;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- Presentacion/Presentacion/FrmLogin.cs
+++ Presentacion/Presentacion/FrmLogin.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Presentacion
 {
-    public partial class CrearUsuario : Form
+    public partial class FrmLogin : Form
     {
-        public CrearUsuario()
+        public FrmLogin()
         {
             InitializeComponent();
         }
