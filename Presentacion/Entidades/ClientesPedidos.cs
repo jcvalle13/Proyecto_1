e826510746cdc2ee97e57cf8622e1e1a1f5bd200
Steph <Steph@STEPH>
2020-07-25 20:22:28 -0600

    creacion entidades ClientesPedidos

---
+++ Presentacion/Entidades/ClientesPedidos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class ClientesPedidos
+    {
+        public string Identificacion { get; set; }
+        public string Nombre { get; set; }
+        public string Producto { get; set; }
+        public string Direccion { get; set; }
+        public bool Modo_Pago { get; set; }
+
+        public ClientesPedidos()
+
+        {
+            Identificacion = string.Empty;
+            Nombre = string.Empty;
+            Producto = string.Empty;
+            Direccion = string.Empty;
+            Modo_Pago = false;
+        }
+    }
+}
